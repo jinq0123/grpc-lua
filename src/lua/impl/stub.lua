35c2a44166a2de1d5fbdd7024614162b6f528bf9
Jin Qing <jinq0123@163.com>
2017-04-15 14:43:01 +0800

    Add request().

--- src/lua/impl/stub.lua
+++ src/lua/impl/stub.lua
@@ -14,4 +14,21 @@ function Stub:set_service_name(service_name)
 	self.service_name = service_name
 end  -- set_service_name()
 
+-- Set timeout seconds like 0.5s.
+-- nil timeout means no timeout.
+function Stub:set_timeout_sec(timeout_sec)
+	self.timeout_sec = timeout_sec
+end  -- set_timeout_sec()
+
+-- Blocking request. Return the response.
+function Stub:request(method_name, request)
+	return {}  -- XXX
+end  -- request()
+
+-- Async request.
+function Stub:async_request(method_name, request, on_response)
+	assert(nil == on_response or "function" == type(on_response))
+	-- XXX
+end  -- async_request()
+
 return Stub
