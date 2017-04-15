c393318063d8e16f3eefec2cb318f54961b19682
Jin Qing <jinq0123@163.com>
2017-04-15 15:02:42 +0800

    Add register().

--- src/lua/grpc_lua.lua
+++ src/lua/grpc_lua.lua
@@ -3,11 +3,20 @@
 local M = {}
 
 local c = require("grpc_lua.c")  -- from grpc_lua.so
+local pb = require("protobuf")
 local Stub = require("impl.stub")
 
 function M.test()
 	c.test()
 end  -- test()
+
+function M.register(buffer)
+	pb.register(buffer)
+end
+
+function M.register_file(file_name)
+	pb.register(file_name)
+end
 
 -- Input "host:port" like: "a.b.com:6666" or "1.2.3.4:6666".
 function M.Channel(host_port)
