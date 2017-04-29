7e17483704792ee3f1c253f251832452c8d80c58
Jin Qing <jinq0123@163.com>
2017-04-29 18:15:56 +0800

    Update grpc_lua.lua.

--- src/lua/grpc_lua.lua
+++ src/lua/grpc_lua.lua
@@ -7,25 +7,31 @@ local pb = require("luapbintf")
 local ServiceStub = require("impl.service_stub")
 
 function M.test()
-	c.test()
+    c.test()
 end  -- test()
 
-function M.register(buffer)
-	pb.register(buffer)
-end
+function M.add_proto_path(proto_path)
+    pb.add_proto_path(proto_path)
+end  -- add_proto_path()
 
-function M.register_file(file_name)
-	pb.register_file(file_name)
+function M.map_path(virtual_path, disk_path)
+    pb.map_path(virtual_path, disk_path)
+end  -- map_path()
+
+-- file_name must be relative to proto path.
+-- e.g. import_proto_file("foo.proto")
+function M.import_proto_file(file_name)
+    pb.import_proto_file(file_name)
 end
 
 -- Input "host:port" like: "a.b.com:6666" or "1.2.3.4:6666".
 function M.Channel(host_port)
-	assert("string" == type(host_port))
-	return c.Channel(host_port)
+    assert("string" == type(host_port))
+    return c.Channel(host_port)
 end  -- Channel()
 
 function M.ServiceStub(channel)
-	return ServiceStub:new(channel)
+    return ServiceStub:new(channel)
 end  -- Stub()
 
 return M
--- src/lua/impl/service_stub.lua
+++ src/lua/impl/service_stub.lua
@@ -3,36 +3,38 @@ local ServiceStub = {}
 local c = require("grpc_lua.c")  -- from grpc_lua.so
 
 function ServiceStub:new(channel)
-	local stub = {
-		c_stub = c.ServiceStub(channel),
-		channel = channel,
-	}
-	setmetatable(stub, self)
-	self.__index = self
-	return stub
+    local stub = {
+        c_stub = c.ServiceStub(channel),
+        channel = channel,
+    }
+    setmetatable(stub, self)
+    self.__index = self
+    return stub
 end  -- new()
 
 -- service_name is full name like "helloworld.Greeter".
 function ServiceStub:set_service_name(service_name)
-	self.service_name = service_name
+    self.service_name = service_name
 end  -- set_service_name()
 
 -- Set timeout seconds like 0.5s.
 -- nil timeout means no timeout.
 function ServiceStub:set_timeout_sec(timeout_sec)
-	self.timeout_sec = timeout_sec
+    self.timeout_sec = timeout_sec
 end  -- set_timeout_sec()
 
 -- Blocking request. Return the response.
 function ServiceStub:request(method_name, request)
-	self.c_stub.request()
-	return {}  -- XXX
+    assert("table" == type(request))
+    self.c_stub.request()
+    return {}  -- XXX
 end  -- request()
 
 -- Async request.
 function ServiceStub:async_request(method_name, request, on_response)
-	assert(nil == on_response or "function" == type(on_response))
-	-- XXX
+    assert("table" == type(request))
+    assert(nil == on_response or "function" == type(on_response))
+    -- XXX
 end  -- async_request()
 
 return ServiceStub
