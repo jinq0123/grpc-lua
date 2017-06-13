8fc6eef95d23ef8992130fe516c34ebe3195aba1
Jin Qing <jinq0123@163.com>
2017-06-13 16:54:31 +0800

    Convert tab to space.

--- examples/helloworld/greeter_client.lua
+++ examples/helloworld/greeter_client.lua
@@ -6,22 +6,22 @@ package.path = "../../src/lua/?.lua;" .. package.path
 local grpc = require("grpc_lua")
 
 function main()
-	grpc.import_proto_file("helloworld.proto")
+    grpc.import_proto_file("helloworld.proto")
 
-	local ch = grpc.Channel("localhost:50051")
-	local stub = grpc.ServiceStub(ch)
-	stub:set_service_name("helloworld.Greeter")
+    local ch = grpc.Channel("localhost:50051")
+    local stub = grpc.ServiceStub(ch)
+    stub:set_service_name("helloworld.Greeter")
 
-	-- Blocking request.
-	local request = { name = "world" }
-	local response = stub:request("SayHello", request)
-	print("Greeter received: " .. response.message)
-	
-	-- Async request.
-	stub:async_request("SayHello", request, function(response)
-		print("Async greeter received: " .. response.message)
-	end)
-	stub:blocking_run()
+    -- Blocking request.
+    local request = { name = "world" }
+    local response = stub:request("SayHello", request)
+    print("Greeter received: " .. response.message)
+    
+    -- Async request.
+    stub:async_request("SayHello", request, function(response)
+        print("Async greeter received: " .. response.message)
+    end)
+    stub:blocking_run()
 end  -- main()
 
 main()
