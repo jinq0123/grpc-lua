230915c80df496728f4b595fd420ad7ba8ba553f
Jin Qing <jinq0123@163.com>
2017-08-09 18:45:19 +0800

    Update bat.

--- examples/conan_install.bat
+++ examples/conan_install.bat
@@ -7,7 +7,7 @@ REM Ignore if ERROR: Remote 'jinq0123' already exists with same URL
 conan remote add remote_bintray_jinq0123 https://api.bintray.com/conan/jinq0123/test
 
 REM Using debug_x64
-arg=--build missing -s build_type=Debug -s arch=x86_64 -s compiler.runtime=MDd
+set arg=--build missing -s build_type=Debug -s arch=x86_64 -s compiler.runtime=MDd
 
 conan install luapbintf/0.1@jinq0123/testing %arg%
 conan install grpc-lua/0.1@jinq0123/testing %arg%
@@ -16,12 +16,16 @@ REM Ignore if ERROR: Remote 'remote_bintray_jinq0123' not found in remotes
 conan remote remove remote_bintray_jinq0123
 
 REM Copy exe and dlls.
-set lua_cpp_exe=%conandata%\lua-cpp\5.3.4\jinq0123\testing\package\c83d8b197f1a331ca3b55943846d427ad4f7f8e1\bin\lua-cpp.exe
+set lua_cpp_bin=%conandata%\lua-cpp\5.3.4\jinq0123\testing\package\c83d8b197f1a331ca3b55943846d427ad4f7f8e1\bin
+set lua_cpp_exe=%lua_cpp_bin%\lua-cpp.exe
+set lua_cpp_dll=%lua_cpp_bin%\lua-cpp.dll
 copy "%lua_cpp_exe%" helloworld\
 copy "%lua_cpp_exe%" route_guide\
+copy "%lua_cpp_dll%" helloworld\
+copy "%lua_cpp_dll%" route_guide\
 set luapbintf_dll=%conandata%\luapbintf\0.1\jinq0123\testing\package\33364f43a1ad585f1513b55ab7b5a8ab2b38e187\bin\luapbintf.dll
-set grpc_lua_dll=%conandata%\grpc-lua\0.1\jinq0123\testing\package\c83d8b197f1a331ca3b55943846d427ad4f7f8e1\bin\grpc-lua.dll
-REM or set grpc_lua_dll=..\..\premake\vs2015\bin\x64\Debug\grpc-lua.dll
+set grpc_lua_dll=%conandata%\grpc-lua\0.1\jinq0123\testing\package\9081c368755c2f201c52a7a4fca3379da109d70f\bin\grpc_lua.dll
+REM or set grpc_lua_dll=..\..\premake\vs2015\bin\x64\Debug\grpc_lua.dll
 copy "%luapbintf_dll%" helloworld\
 copy "%luapbintf_dll%" route_guide\
 copy "%grpc_lua_dll%" helloworld\
--- examples/helloworld/run_client.bat
+++ examples/helloworld/run_client.bat
@@ -1,4 +1,6 @@
-call ..\..\conan_install.bat
+pushd ..
+call conan_install.bat
+popd
 
 lua-cpp.exe greeter_client.lua
 
