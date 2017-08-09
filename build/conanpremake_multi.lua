1317de394ffe0001224a7f1760127a85b85c8c95
Jin Qing <jinq0123@163.com>
2017-08-09 11:32:45 +0800

    Fix conanpremake_multi.lua.

--- build/conanpremake_multi.lua
+++ build/conanpremake_multi.lua
@@ -1,14 +1,21 @@
 
-	filter { "configurations:Debug", "platforms:x32" }
-		require("conanpremake_debug_x32")
-	filter { "configurations:Debug", "platforms:x64" }
-		require("conanpremake_debug_x64")
-	filter { "configurations:Release", "platforms:x32" }
-		require("conanpremake_release_x32")
-	filter { "configurations:Release", "platforms:x64" }
-		require("conanpremake_release_x64")
-	filter {}
-
+local function conan_basic_setup()
 	includedirs { conan_includedirs }
 	libdirs { conan_libdirs }
 	links { conan_libs }
+end  -- conan_basic_setup
+
+	filter { "configurations:Debug", "platforms:x32" }
+		require("conanpremake_debug_x32")
+		conan_basic_setup()
+	filter { "configurations:Debug", "platforms:x64" }
+		require("conanpremake_debug_x64")
+		conan_basic_setup()
+	filter { "configurations:Release", "platforms:x32" }
+		require("conanpremake_release_x32")
+		conan_basic_setup()
+	filter { "configurations:Release", "platforms:x64" }
+		require("conanpremake_release_x64")
+		conan_basic_setup()
+	filter {}
+
