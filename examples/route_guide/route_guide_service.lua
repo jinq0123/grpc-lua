cd11e1374d02ed6878792d8a0b656ea6ec749ec4
Jin Qing <jinq0123@163.com>
2017-10-08 15:04:27 +0800

    ListFeatures().

--- examples/route_guide/route_guide_service.lua
+++ examples/route_guide/route_guide_service.lua
@@ -36,7 +36,28 @@ end  -- GetFeature()
 function M.ListFeatures(rectangle, writer)
     assert("table" == type(rectangle))
     assert("table" == type(writer))
-    -- XXX
+    local lo = assert(rectangle.lo)
+    local hi = assert(rectangle.hi)
+    local left = math.min(lo.longitude, hi.longitude)
+    local right = math.max(lo.longitude, hi.longitude)
+    local top = math.max(lo.latitude, hi.latitude)
+    local bottom = math.min(lo.latitude, hi.latitude);
+
+    -- Todo: write in thread...
+
+    for _, f in ipairs(db.features) do
+        local l = f.location
+        if l.longitude >= left and
+           l.longitude <= right and
+           l.latitude >= bottom and
+           l.latitude <= top then
+            if not writer.write(f) then
+                print("Failed to write.")
+                break
+            end  -- if not writer.write()
+        end  -- if l
+    end  -- for _, f
+    writer.close()
 end  -- ListFeatures()
 
 return M
