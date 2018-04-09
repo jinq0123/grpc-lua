f405b8dab02e797f65f07a9a60f21ad58422b1d7
Jin Qing <jinq0123@163.com>
2018-04-09 11:14:26 +0800

    Add lib cares to fix undefined reference to `ares_free_data'.

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 #include "lua.h"
+#include "lauxlib.h"  
+#include "lualib.h"  
 
 extern "C"
 int luaopen_grpc_lua_c(lua_State* L);
 
 int main() {
-	void * p = static_cast<void*>(luaopen_grpc_lua_c);
+	lua_State *L = luaL_newstate();
+	luaopen_grpc_lua_c(L);
 	return 0;
 }
